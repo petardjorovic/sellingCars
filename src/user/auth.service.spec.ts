35c64863edc8adb18a151b670ffcb6c865c349b2
petardjorovic <perodjorovic@gmail.com>
2026-06-01 22:48:37 +0200

    authService unit tests

--- src/user/auth.service.spec.ts
+++ src/user/auth.service.spec.ts
@@ -2,21 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { User } from './user.entity';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { it } from 'node:test';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import * as argon2 from 'argon2';
+
+jest.mock('argon2');
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let mockUserService: Partial<UserService>;
 
   beforeEach(async () => {
-    const fakeUserService: Partial<UserService> = {
-      find: () => Promise.resolve([]),
-      create: (data: CreateUserDto) =>
-        Promise.resolve({
-          id: 1,
-          email: data.email,
-          password: data.password,
-        } as User),
+    jest.clearAllMocks();
+    mockUserService = {
+      find: jest.fn().mockResolvedValue([]),
+      create: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -24,24 +27,135 @@ describe('AuthService', () => {
         AuthService,
         {
           provide: UserService,
-          useValue: fakeUserService,
+          useValue: mockUserService,
         },
       ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 
+  //* SIGN UP
   it('creates new user with hashed password', async () => {
-    const user = await service.signUp({
-      email: 'test@test.com',
+    (argon2.hash as jest.Mock).mockResolvedValue('hashed-password');
+    (mockUserService.create as jest.Mock).mockResolvedValue({
+      id: 1,
+      email: 'test@email.com',
+      password: 'hash',
+    } as User);
+
+    const user = await authService.signUp({
+      email: 'test@email.com',
       password: '123456',
     });
 
-    expect(user.password).not.toEqual('123456');
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('test@email.com');
+    expect(mockUserService.create).toHaveBeenCalledTimes(1);
+    expect(mockUserService.create).toHaveBeenCalledWith({
+      email: 'test@email.com',
+      password: 'hashed-password',
+    });
+    expect(argon2.hash).toHaveBeenCalledTimes(1);
+    expect(argon2.hash).toHaveBeenCalledWith('123456');
+    expect(user).toBeDefined();
+    expect(user.email).toEqual('test@email.com');
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    (mockUserService.find as jest.Mock).mockResolvedValue([
+      {
+        id: 1,
+        email: 'test@email.com',
+        password: 'hash',
+      } as User,
+    ]);
+
+    await expect(
+      authService.signUp({
+        email: 'test@email.com',
+        password: '123456',
+      }),
+    ).rejects.toThrow(BadRequestException);
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('test@email.com');
+    expect(argon2.hash).not.toHaveBeenCalled();
+    expect(mockUserService.create).not.toHaveBeenCalled();
+  });
+
+  it('throws an error if password hashing fails', async () => {
+    (argon2.hash as jest.Mock).mockRejectedValue(new Error());
+
+    await expect(
+      authService.signUp({ email: 'test@email.com', password: '123456' }),
+    ).rejects.toThrow(InternalServerErrorException);
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('test@email.com');
+    expect(argon2.hash).toHaveBeenCalledTimes(1);
+    expect(argon2.hash).toHaveBeenCalledWith('123456');
+    expect(mockUserService.create).not.toHaveBeenCalled();
+  });
+
+  //* SIGN IN
+  it('throws an error if user not found', async () => {
+    await expect(
+      authService.signIn({ email: 'missing@test.com', password: '123456' }),
+    ).rejects.toThrow(NotFoundException);
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('missing@test.com');
+    expect(argon2.verify).not.toHaveBeenCalled();
+  });
+
+  it('throws an error if password is invalid', async () => {
+    (argon2.verify as jest.Mock).mockResolvedValue(false);
+    (mockUserService.find as jest.Mock).mockResolvedValue([
+      { id: 1, email: 'test@email.com', password: 'hash' } as User,
+    ]);
+
+    await expect(
+      authService.signIn({ email: 'test@email.com', password: '123456' }),
+    ).rejects.toThrow(BadRequestException);
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('test@email.com');
+    expect(argon2.verify).toHaveBeenCalledTimes(1);
+    expect(argon2.verify).toHaveBeenCalledWith('hash', '123456');
+  });
+
+  it('throws an error if password verifying fails', async () => {
+    (argon2.verify as jest.Mock).mockRejectedValue(new Error());
+    (mockUserService.find as jest.Mock).mockResolvedValue([
+      { id: 1, email: 'test@email.com', password: 'hash' } as User,
+    ]);
+
+    await expect(
+      authService.signIn({ email: 'test@email.com', password: '123456' }),
+    ).rejects.toThrow(InternalServerErrorException);
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('test@email.com');
+    expect(argon2.verify).toHaveBeenCalledTimes(1);
+    expect(argon2.verify).toHaveBeenCalledWith('hash', '123456');
+  });
+
+  it('signs in and returns user if email and password is correct', async () => {
+    (argon2.verify as jest.Mock).mockResolvedValue(true);
+    (mockUserService.find as jest.Mock).mockResolvedValue([
+      { id: 1, email: 'test@email.com', password: 'hash' } as User,
+    ]);
+    const user = await authService.signIn({
+      email: 'test@email.com',
+      password: '123456',
+    });
+
+    expect(user).toBeDefined();
+    expect(user.email).toEqual('test@email.com');
+    expect(user.password).toEqual('hash');
+    expect(mockUserService.find).toHaveBeenCalledTimes(1);
+    expect(mockUserService.find).toHaveBeenCalledWith('test@email.com');
+    expect(argon2.verify).toHaveBeenCalledTimes(1);
+    expect(argon2.verify).toHaveBeenCalledWith('hash', '123456');
   });
 });
